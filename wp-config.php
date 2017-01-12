b82c7de8ed623f9ccfaa8e372def822a9bd7fad7
vikas satpute <vikas satpute>
2017-01-12 15:28:40 +0530

    db config

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wpangular');
+define('DB_NAME', 'heroku_543929b3e4a033a');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'bd57fd4651dfdd');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '9a44b5ceed8aae4 ');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
